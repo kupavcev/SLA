6fcf05e24d0aca752bbd6e4f8bcd234f1707a510
kupavcev <kupavcev@unact.ru>
2014-02-11 09:30:27 +0300

    Update populate_sla_monitor_rc_unact.sql

--- proc/populate_sla_monitor_rc_unact.sql
+++ proc/populate_sla_monitor_rc_unact.sql
@@ -1,2 +1,3 @@
 set dateformat dmy;
 exec calculate_sla_monitor_rc_unact '01.02.2013';--раньше не было итема в заббиксе
+exec calculate_sla_monitor_rc_unact2 '01.02.2014';-- до 01.02.2014 сла считалось из monitor.unact.ru, после из monitor2.unact.ru
